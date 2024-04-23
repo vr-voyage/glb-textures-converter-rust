df149b7f36b83669f98e635d7d4eaad8abe836b7
Voyage <voyage@dontcontactmebymail.com>
2024-04-24 00:29:47 +0200

    Make it useable from a right-click
    Converting the whole thing to WASM is just "not possible".
    So the idea is to able to summon the converter from a right-click.
    
    And tomorrow, I'll explore Javascript lands in quest for
    a converter that could work on the web.
    
    Signed-off-by: Voyage <voyage@dontcontactmebymail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use chrono::Local;
 use std::io::Write;
 use std::env;
 use std::fs::OpenOptions;
+use std::path::{Path,PathBuf};
 
 mod converter;
 
@@ -11,19 +13,34 @@ mod converter;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 3
+    let n_args = args.len();
+    if n_args < 2
     {
-        println!("Usage : {} file.glb out.glb", args[0]);
+        println!("Usage : {} file.glb [out.glb]", args[0]);
         return;
     }
 
     let glb_filename = &args[1];
-    let output_filename = &args[2];
+    let output_filepath:PathBuf;
+
+    if n_args >= 3
+    {
+        output_filepath = PathBuf::from(&args[2]);
+    }
+    else
+    {
+        let input_path = Path::new(glb_filename);
+        let glb_filename_without_ext:&str = input_path.file_stem().unwrap().to_str().unwrap();
+        let current_date = Local::now();
+        let current_time = current_date.format("%Y%m%d%H%M%S");
+        let output_filename = format!("{}-{}-converted.glb", glb_filename_without_ext, current_time);
+        output_filepath = input_path.with_file_name(output_filename);
+    }
 
     match std::fs::read(glb_filename) {
         Ok(bytes) => { 
             let out_glb = converter::create_new_glb_with_converted_textures(bytes);
-            let mut f = OpenOptions::new().write(true).create(true).open(output_filename).unwrap();
+            let mut f = OpenOptions::new().write(true).create(true).open(output_filepath).unwrap();
             let _ = f.write(&out_glb[..]);
             let _ = f.sync_all().unwrap();
         }

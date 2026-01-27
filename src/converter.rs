dc3f7d720403938a0a85f403a171d61006e0a54c
Voyage <voyage@miouyouyou.fr>
2026-01-27 20:47:21 +0100

    Getting rid of non multiple of 4 errors
    Which mean that the converter can now handle ANY
    kind of VRM texture ! YAAAAY !
    
    1 year for that...
    
    Signed-off-by: Voyage <voyage@miouyouyou.fr>

--- src/converter.rs
+++ src/converter.rs
@@ -1,4 +1,5 @@
 use bytes::Buf;
+use intel_tex_2::bc5;
 use core::mem::size_of;
 use ddsfile::{AlphaMode, Caps2, D3D10ResourceDimension, Dds, DxgiFormat, NewDxgiParams};
 use image::io::Reader;
@@ -246,6 +247,8 @@ fn mark_bufferview_used_by_images(images: &Vec<Image>, buffer_views: &mut Vec<Bu
 enum CompressionFormat
 {
     RGBA8,
+    RGBA8_UNORM,
+    DXT5,
     BC7
 }
 
@@ -253,8 +256,20 @@ impl fmt::Display for CompressionFormat {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             CompressionFormat::RGBA8 => write!(f, "RGBA8"),
+            CompressionFormat::DXT5 => write!(f, "DXT5"),
             CompressionFormat::BC7 => write!(f, "BC7"),
+            CompressionFormat::RGBA8_UNORM => write!(f, "RGBA8_UNORM")
         }
+    }
+}
+
+fn compresion_format_to_dxgi_format(compression_format:&CompressionFormat) -> DxgiFormat
+{
+    return match compression_format {
+        CompressionFormat::BC7 => DxgiFormat::BC7_UNorm_sRGB,
+        CompressionFormat::RGBA8 => DxgiFormat::R8G8B8A8_UInt,
+        CompressionFormat::RGBA8_UNORM => DxgiFormat::R8G8B8A8_UNorm,
+        CompressionFormat::DXT5 => DxgiFormat::BC5_UNorm,
     }
 }
 
@@ -329,7 +344,9 @@ fn convert_image_as_raw_rgba8(width: u32, height: u32, rgba8_content: &[u8]) -> 
     return (width, height, rgba8_content.to_vec(), CompressionFormat::RGBA8);
 }
 
-fn convert_image_as_bc7(width: u32, height: u32, rgba8_content: &[u8])  -> (u32, u32, Vec<u8>, CompressionFormat)
+type SurfaceHandler = fn(&intel_tex_2::RgbaSurface, &mut [u8]);
+
+fn convert_image_as(width: u32, height: u32, rgba8_content: &[u8], format: DxgiFormat, surface_handler: SurfaceHandler) -> (u32, u32, Vec<u8>, CompressionFormat)
 {
     let block_count = intel_tex_2::divide_up_by_multiple(width * height, 16);
     println!("Block count: {}", block_count);
@@ -338,7 +355,7 @@ fn convert_image_as_bc7(width: u32, height: u32, rgba8_content: &[u8])  -> (u32,
         height,
         width,
         depth: Some(1),
-        format: DxgiFormat::BC7_UNorm,
+        format: format, //  DxgiFormat::BC7_UNorm,
         mipmap_levels: Some(1),
         array_layers: Some(1),
         caps2: Some(Caps2::empty()),
@@ -348,7 +365,7 @@ fn convert_image_as_bc7(width: u32, height: u32, rgba8_content: &[u8])  -> (u32,
     };
     // BC7
     let mut dds = Dds::new_dxgi(NewDxgiParams {
-        format: DxgiFormat::BC7_UNorm,
+        format: format,  // DxgiFormat::BC7_UNorm,
         ..dds_defaults
     })
     .unwrap();
@@ -358,19 +375,49 @@ fn convert_image_as_bc7(width: u32, height: u32, rgba8_content: &[u8])  -> (u32,
         stride: width * 4,
         data: rgba8_content,
     };
-
+    surface_handler(&surface, dds.get_mut_data(0 /* layer */).unwrap());
     println!("Compressing to BC7...");
-    bc7::compress_blocks_into(
-        &bc7::alpha_ultra_fast_settings(),
-        &surface,
-        dds.get_mut_data(0 /* layer */).unwrap(),
-    );
+    
     println!("  Done!");
 
     //dds.write(&mut OpenOptions::new().write(true).create(true).open("a.dds").unwrap());
     let dds_data = dds.get_data(0).unwrap();
     return (width, height, dds_data.to_vec(), CompressionFormat::BC7);
-    
+}
+
+fn align_on(pow2_value: u32, val: u32) -> u32
+{
+    let mask: u32 = pow2_value - 1;
+    return (val+mask) & (!mask);
+}
+
+fn surface_treatment_none(_surface: &intel_tex_2::RgbaSurface, _blocks: &mut [u8])
+{
+
+}
+
+fn surface_treatment_dxt5(surface: &intel_tex_2::RgbaSurface, blocks: &mut [u8])
+{
+    bc5::compress_blocks_into(surface, blocks);
+}
+
+fn surface_treatment_bc7(surface: &intel_tex_2::RgbaSurface, blocks: &mut [u8])
+{
+    bc7::compress_blocks_into(
+        &bc7::alpha_ultra_fast_settings(),
+        &surface,
+        blocks // dds.get_mut_data(0 /* layer */).unwrap(),
+    );
+}
+
+fn surface_handler_for(compression_format:&CompressionFormat) -> SurfaceHandler
+{
+    return match compression_format {
+        CompressionFormat::BC7 => surface_treatment_bc7,
+        CompressionFormat::RGBA8 => surface_treatment_none,
+        CompressionFormat::RGBA8_UNORM => surface_treatment_none,
+        CompressionFormat::DXT5 => surface_treatment_dxt5,
+    }
 }
 
 /**
@@ -382,18 +429,32 @@ fn convert_image_as_bc7(width: u32, height: u32, rgba8_content: &[u8])  -> (u32,
 fn convert_image_content_in(buffer: &[u8]) -> (u32, u32, Vec<u8>, CompressionFormat)
 {
     let img = Reader::new(Cursor::new(buffer)).with_guessed_format().unwrap().decode().unwrap();
-    let rgba8_image = img.to_rgba8();
-    let width = rgba8_image.width();
-    let height = rgba8_image.height();
-    let rgba8_content = &rgba8_image.into_raw()[..];
+    
+    let width = img.width();
+    let height = img.height();
 
     if (width * height) < (256*256)
     {
+        let rgba8_image = img.to_rgba8();
+        let rgba8_content = &rgba8_image.into_raw()[..];
         return convert_image_as_raw_rgba8(width, height, rgba8_content);
     }
     else
     {
-        return convert_image_as_bc7(width, height, rgba8_content);
+        let compression_format = CompressionFormat::BC7;
+        let dxgi_format = compresion_format_to_dxgi_format(&compression_format);
+        let surface_handler = surface_handler_for(&compression_format);
+
+        let used_width = align_on(4, width);
+        let used_height  = align_on(4, height);
+        let resize_filter:image::imageops::FilterType = image::imageops::FilterType::Lanczos3;
+
+        let needs_resize :bool = (used_width != width) | (used_height != height);
+        let used_img: image::DynamicImage = if needs_resize { img.resize_exact(used_width, used_height, resize_filter) } else { img };
+
+        let rgba8_image = used_img.to_rgba8();
+        let rgba8_content = &rgba8_image.into_raw()[..];
+        return convert_image_as(used_width, used_height, rgba8_content, dxgi_format, surface_handler);
     }
     
 
